84bfa8a03d1469f0f2f77231a6cb6b5af91262c1
rafaelpardor <rafaelpardor@protonmail.com>
2019-11-30 16:01:24 -0500

    Data types

---
+++ cpp-platzi/02-types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 20 << std::endl;
+    std::cout << 20.123 << std::endl;
+    std::cout << true << std::endl;
+    std::cout << false << std::endl;
+    std::cout << 'R' << std::endl;
+    std::cout << "Rafael" << std::endl;    
+    return 0;
+}
