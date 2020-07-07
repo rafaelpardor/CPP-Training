22d448de063d555459f1738c93450edc32c36d0b
rafaelpardor <rafaelpardor@protonmail.com>
2020-07-07 01:51:20 -0400

    [UPDATE] Abstrac class

--- datastructure-and-algorithms/ch1-introduction/1.9-abstract_shape_class.cpp
+++ datastructure-and-algorithms/ch1-introduction/1.9-abstract_shape_class.cpp
@@ -47,16 +47,67 @@ double Circle::perimeter()
     return 2 * M_PI * radius;
 }
 
-// Class Rectancle: public Shape{};
+class Rectangle : public Shape
+{
+private:
+    double width = 0, length = 0;
+
+public:
+    Rectangle();
+    Rectangle(double w, double l);
+    virtual void setWidth(double w);
+    virtual void setLength(double l);
+    virtual double area() override;
+    virtual double perimeter() override;
+};
+
+Rectangle::Rectangle()
+{
+    Rectangle(1, 1);
+}
+
+Rectangle::Rectangle(double w, double l)
+{
+    width = w;
+    length = l;
+}
+
+void Rectangle::setWidth(double w)
+{
+    width = w;
+}
+void Rectangle::setLength(double l)
+{
+    length = l;
+}
+
+double Rectangle::area()
+{
+    return width * length;
+}
+
+double Rectangle::perimeter()
+{
+    return 2 * (width + length);
+}
 
 int main()
 {
     double radius = 10;
-    Shape *circle_var = new Circle(radius);
+    Shape *circle_obj = new Circle(radius);
     std::cout << "CIRCLE" << std::endl;
-    std::cout << "Circle radius: "
-              << radius << ". Area: " << circle_var->area()
-              << ". Perimeter: " << circle_var->perimeter()
+    std::cout << "Circle radius: " << radius
+              << " Area: " << circle_obj->area()
+              << " Perimeter: " << circle_obj->perimeter()
+              << "." << std::endl;
+
+    double width = 2, length = 3;
+    Shape *rectangle_obj = new Rectangle(width, length);
+    std::cout << "RECTANGLE" << std::endl;
+    std::cout << " Rectangle width: " << width
+              << " Rectangle length: " << length
+              << " Area: " << rectangle_obj->area()
+              << " Perimeter: " << rectangle_obj->perimeter()
               << "." << std::endl;
     return 0;
 }
