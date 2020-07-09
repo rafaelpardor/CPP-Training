0253d2e83d1239505a27106b5c82e26e55db428a
rafaelpardor <rafaelpardor@protonmail.com>
2020-07-08 22:33:20 -0400

    If you, somehow are reading this I want to kill myself

---
+++ cpp-platzi/area-calculation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+float squareArea(float a) { return a * a; }
+
+int main()
+{
+    float area = 2.2;
+    std::cout << "Area de un cuadreado: " << squareArea(area) << std::endl;
+    return 0;
+}
